751511c5a0bb00829be3011692e0a97ef895c2a7
Tinkerer <your_em/clear>
2019-07-19 20:07:16 +0100

    spirit

---
+++ storage/framework/views/453c8f3e2968d5ef6370f478f06e5d5f976c2204.php
@@ -0,0 +1,10 @@
+<?php $__env->startComponent('mail::message'); ?>
+# Welcome to freeCodeGram
+
+This is a community of fellow developers and we love that you have joined us.
+
+
+All the best,<br>
+Victor
+<?php echo $__env->renderComponent(); ?>
+<?php /**PATH C:\Users\Who else but me\Desktop\freeCodeGram-master\resources\views/emails/welcome-email.blade.php ENDPATH**/ ?>
